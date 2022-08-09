77f27ea058d489c0440dc14a7f3f7e0172afed0a
Frankbosconeven <Frankbosconeven@gmail.com>
2022-08-09 14:02:32 +0000

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return <h1>My first component</h1>;
+    //Before you start any work have to export and inport
+    //Default export and name export
+    // name export is the same name you export can be change
+    // with default export can be export only function
+
+}
+
+export default Header;
---
+++ src/components/Image.jsx
@@ -0,0 +1,4 @@
+import fbb from "../assets/image/fbb.jpg"
+export const Image = () => {
+    return <img src={fbb} alt="dd" />
+}
